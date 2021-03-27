4d2c8314fce9927694ca2558bdce07c9c938b191
KittyKittyKat <ivasam248@gmail.com>
2021-03-27 15:03:42 +0700

    Здесь все точно также как и в Lab2_OOP.

--- Lab4_proc/wisdom_IN_OUT.cpp
+++ Lab4_proc/wisdom_IN_OUT.cpp
@@ -51,253 +51,15 @@ namespace collection_of_wisdom {
 		case 1:
 			sp = new wisdom;
 			sp->k = wisdom::key::APHORISM;
-			////////////////////////////////////////////////////////////// ищем Expression
-			do {
-				getline(ifst, Full_Line); //Строка заносится в Full_Line
-				Data = FindData("Expression:", Full_Line);
-				Exit_Flag = true;
-
-				if (Data.compare("error") == 0) { //если выражения нет, то выбрасываем послед. строки и переходим к новой мудрости
-					delete[] sp;
-					string Junk; //для мусора
-					getline(ifst, Junk); //Здесь - уникальная характеристика
-					getline(ifst, Junk); //Здесь - оценка
-					Junk.clear();
-					return 0;
-				}
-				else if (Data.compare("empty") == 0) {//если пустая строка
-					Exit_Flag = false; // если false, то продолжаем цикл
-				}
-
-			} while ((ifst.eof() == false) && (Exit_Flag == false));
-
-			if (ifst.eof()) {
-				cout << "INFORMATION: the end of file." << endl;
-				return 0;
-			}
-			else { //если не конец файла
-				*sp->expression = CheckForOverflow(Data, sp->expression, ifst, 100);// проверка выражения на переполнение, занесение выражения в expression
-			}
-			////////////////////////////////////////////////ищем уникальную характеристику
-			if (In(sp->a, ifst) == "error") {
-				return 0;
-			}
-
-			if (ifst.eof()) {//если конец файла, то выход
-				return 0;
-			}//иначе продолжаем
-			//////////////////////////////////////////////ищем оценку
-			do {//ищем оценку
-				getline(ifst, Full_Line);
-				Data = FindData("Rate:", Full_Line);
-
-				if (Data.compare("error") != 0 && Data.compare("empty") != 0) {// если НЕ "ошибка"(данные не введены) И НЕ "пусто"(пустая строка)
-					try {
-						if (isdigit(Data[0]) == 0) {
-							throw Data;//если Data - строка, а не число
-						}
-						if (static_cast<int>(stoi(Data, 0, 10)) > 10) {// если переполнение
-							delete[] sp;
-							cout << "ERROR: unexpected value (Rate > 10)." << endl;
-							return 0;
-						}
-						else if (static_cast<int>(stoi(Data, 0, 10)) < 0) {// если слишком маленькое значение
-							delete[] sp;
-							cout << "ERROR: unexpected value (Rate < 10)." << endl;
-							return 0;
-						}
-						else {//если с оценкой все хорошо
-							sp->rate = static_cast<int>(stoi(Data, 0, 10));
-							//Exit_Flag = true;
-							return sp;
-						}
-					}
-					catch (string Data) {
-						cout << "ERROR: 'Rate' should be int ('Rate' == " << Data << ")." << endl;
-						delete[] sp;
-						return 0;
-					}
-
-				}
-				else if (Data.compare("empty") == 0) { // если "пусто"(пустая строка), то идем на следующую строку
-					Exit_Flag = false;
-				}
-			} while ((ifst.eof() == false) && (Exit_Flag == false));
-
-			if (ifst.eof()) {
-				cout << "INFORMATION: the end of file." << endl;
-				return 0;
-			}
-			Full_Line.clear();
-
-			return sp;
-			
+			break;
 		case 2:
 			sp = new wisdom;
 			sp->k = wisdom::key::PROVERB;
-			////////////////////////////////////////////////////////////// ищем Expression
-			do {
-				getline(ifst, Full_Line); //Строка заносится в Full_Line
-				Data = FindData("Expression:", Full_Line);
-				Exit_Flag = true;
-
-				if (Data.compare("error") == 0) { //если выражения нет, то выбрасываем послед. строки и переходим к новой мудрости
-					delete[] sp;
-					string Junk; //для мусора
-					getline(ifst, Junk); //Здесь - уникальная характеристика
-					getline(ifst, Junk); //Здесь - оценка
-					Junk.clear();
-					return 0;
-				}
-				else if (Data.compare("empty") == 0) {//если пустая строка
-					Exit_Flag = false; // если false, то продолжаем цикл
-				}
-
-			} while ((ifst.eof() == false) && (Exit_Flag == false));
-
-			if (ifst.eof()) {
-				cout << "INFORMATION: the end of file." << endl;
-				return 0;
-			}
-			else { //если не конец файла
-				*sp->expression = CheckForOverflow(Data, sp->expression, ifst, 100);// проверка выражения на переполнение, занесение выражения в expression
-			}
-			////////////////////////////////////////////////ищем уникальную характеристику
-			if (In(sp->p, ifst) == "error") {
-				return 0;
-			}
-
-			if (ifst.eof()) {//если конец файла, то выход
-				return 0;
-			}//иначе продолжаем
-			//////////////////////////////////////////////ищем оценку
-			do {//ищем оценку
-				getline(ifst, Full_Line);
-				Data = FindData("Rate:", Full_Line);
-
-				if (Data.compare("error") != 0 && Data.compare("empty") != 0) {// если НЕ "ошибка"(данные не введены) И НЕ "пусто"(пустая строка)
-					try {
-						if (isdigit(Data[0]) == 0) {
-							throw Data;//если Data - строка, а не число
-						}
-						if (static_cast<int>(stoi(Data, 0, 10)) > 10) {// если переполнение
-							delete[] sp;
-							cout << "ERROR: unexpected value (Rate > 10)." << endl;
-							return 0;
-						}
-						else if (static_cast<int>(stoi(Data, 0, 10)) < 0) {// если слишком маленькое значение
-							delete[] sp;
-							cout << "ERROR: unexpected value (Rate < 10)." << endl;
-							return 0;
-						}
-						else {//если с оценкой все хорошо
-							sp->rate = static_cast<int>(stoi(Data, 0, 10));
-							//Exit_Flag = true;
-							return sp;
-						}
-					}
-					catch (string Data) {
-						cout << "ERROR: 'Rate' should be int ('Rate' == " << Data << ")." << endl;
-						delete[] sp;
-						return 0;
-					}
-
-				}
-				else if (Data.compare("empty") == 0) { // если "пусто"(пустая строка), то идем на следующую строку
-					Exit_Flag = false;
-				}
-			} while ((ifst.eof() == false) && (Exit_Flag == false));
-
-			if (ifst.eof()) {
-				cout << "INFORMATION: the end of file." << endl;
-				return 0;
-			}
-			Full_Line.clear();
-
-			return sp;
+			break;
 		case 3:
 			sp = new wisdom;
 			sp->k = wisdom::key::PUZZLE;
-			////////////////////////////////////////////////////////////// ищем Expression
-			do {
-				getline(ifst, Full_Line); //Строка заносится в Full_Line
-				Data = FindData("Expression:", Full_Line);
-				Exit_Flag = true;
-
-				if (Data.compare("error") == 0) { //если выражения нет, то выбрасываем послед. строки и переходим к новой мудрости
-					delete[] sp;
-					string Junk; //для мусора
-					getline(ifst, Junk); //Здесь - уникальная характеристика
-					getline(ifst, Junk); //Здесь - оценка
-					Junk.clear();
-					return 0;
-				}
-				else if (Data.compare("empty") == 0) {//если пустая строка
-					Exit_Flag = false; // если false, то продолжаем цикл
-				}
-
-			} while ((ifst.eof() == false) && (Exit_Flag == false));
-
-			if (ifst.eof()) {
-				cout << "INFORMATION: the end of file." << endl;
-				return 0;
-			}
-			else { //если не конец файла
-				*sp->expression = CheckForOverflow(Data, sp->expression, ifst, 100);// проверка выражения на переполнение, занесение выражения в expression
-			}
-			////////////////////////////////////////////////ищем уникальную характеристику
-			if (In(sp->z, ifst) == "error") {
-				return 0;
-			}
-
-			if (ifst.eof()) {//если конец файла, то выход
-				return 0;
-			}//иначе продолжаем
-			//////////////////////////////////////////////ищем оценку
-			do {//ищем оценку
-				getline(ifst, Full_Line);
-				Data = FindData("Rate:", Full_Line);
-
-				if (Data.compare("error") != 0 && Data.compare("empty") != 0) {// если НЕ "ошибка"(данные не введены) И НЕ "пусто"(пустая строка)
-					try {
-						if (isdigit(Data[0]) == 0) {
-							throw Data;//если Data - строка, а не число
-						}
-						if (static_cast<int>(stoi(Data, 0, 10)) > 10) {// если переполнение
-							delete[] sp;
-							cout << "ERROR: unexpected value (Rate > 10)." << endl;
-							return 0;
-						}
-						else if (static_cast<int>(stoi(Data, 0, 10)) < 0) {// если слишком маленькое значение
-							delete[] sp;
-							cout << "ERROR: unexpected value (Rate < 10)." << endl;
-							return 0;
-						}
-						else {//если с оценкой все хорошо
-							sp->rate = static_cast<int>(stoi(Data, 0, 10));
-							//Exit_Flag = true;
-							return sp;
-						}
-					}
-					catch (string Data) {
-						cout << "ERROR: 'Rate' should be int ('Rate' == " << Data << ")." << endl;
-						delete[] sp;
-						return 0;
-					}
-
-				}
-				else if (Data.compare("empty") == 0) { // если "пусто"(пустая строка), то идем на следующую строку
-					Exit_Flag = false;
-				}
-			} while ((ifst.eof() == false) && (Exit_Flag == false));
-
-			if (ifst.eof()) {
-				cout << "INFORMATION: the end of file." << endl;
-				return 0;
-			}
-			Full_Line.clear();
-
-			return sp;
+			break;
 		default:
 			string Junk; //для мусора
 			getline(ifst, Junk); //Здесь - выражение
@@ -306,8 +68,107 @@ namespace collection_of_wisdom {
 			Junk.clear();
 			return 0;
 		}
-		Full_Line.clear();
-		Data.clear();
+
+		////////////////////////////////////////////////////////////// ищем Expression
+		do {
+			getline(ifst, Full_Line); //Строка заносится в Full_Line
+			Data = FindData("Expression:", Full_Line);
+			Exit_Flag = true;
+
+			if (Data.compare("error") == 0) { //если выражения нет, то выбрасываем послед. строки и переходим к новой мудрости
+				delete[] sp;
+				string Junk; //для мусора
+				getline(ifst, Junk); //Здесь - уникальная характеристика
+				getline(ifst, Junk); //Здесь - оценка
+				Junk.clear();
+				return 0;
+			}
+			else if (Data.compare("empty") == 0) {//если пустая строка
+				Exit_Flag = false; // если false, то продолжаем цикл
+			}
+
+		} while ((ifst.eof() == false) && (Exit_Flag == false));
+
+		if (ifst.eof()) {
+			cout << "INFORMATION: the end of file." << endl;
+			return 0;
+		}
+		else { //если не конец файла
+			*sp->expression = CheckForOverflow(Data, sp->expression, ifst, 100);// проверка выражения на переполнение, занесение выражения в expression
+		}
+		////////////////уникальная характеристика
+		switch (sp->k) {
+		case wisdom::key::APHORISM:
+			if (In(sp->a, ifst) == "error") {
+				return 0;
+			}
+
+			if (ifst.eof()) {//если конец файла, то выход
+				return 0;
+			}//иначе продолжаем
+			break;
+		case wisdom::key::PROVERB:
+			if (In(sp->p, ifst) == "error") {
+				return 0;
+			}
+
+			if (ifst.eof()) {//если конец файла, то выход
+				return 0;
+			}//иначе продолжаем
+			break;
+		case wisdom::key::PUZZLE:
+			if (In(sp->z, ifst) == "error") {
+				return 0;
+			}
+
+			if (ifst.eof()) {//если конец файла, то выход
+				return 0;
+			}//иначе продолжаем
+			break;
+		}
+		//////////////////////////////////////ищем оценку
+		do {//ищем оценку
+			getline(ifst, Full_Line);
+			Data = FindData("Rate:", Full_Line);
+
+			if (Data.compare("error") != 0 && Data.compare("empty") != 0) {// если НЕ "ошибка"(данные не введены) И НЕ "пусто"(пустая строка)
+				try {
+					if (isdigit(Data[0]) == 0) {
+						throw Data;//если Data - строка, а не число
+					}
+					if (static_cast<int>(stoi(Data, 0, 10)) > 10) {// если переполнение
+						delete[] sp;
+						cout << "ERROR: unexpected value (Rate > 10)." << endl;
+						return 0;
+					}
+					else if (static_cast<int>(stoi(Data, 0, 10)) < 0) {// если слишком маленькое значение
+						delete[] sp;
+						cout << "ERROR: unexpected value (Rate < 10)." << endl;
+						return 0;
+					}
+					else {//если с оценкой все хорошо
+						sp->rate = static_cast<int>(stoi(Data, 0, 10));
+						Full_Line.clear();
+						Data.clear();
+						return sp;
+					}
+				}
+				catch (string Data) {
+					cout << "ERROR: 'Rate' should be int ('Rate' == " << Data << ")." << endl;
+					delete[] sp;
+					return 0;
+				}
+
+			}
+			else if (Data.compare("empty") == 0) { // если "пусто"(пустая строка), то идем на следующую строку
+				Exit_Flag = false;
+			}
+		} while ((ifst.eof() == false) && (Exit_Flag == false));
+
+		if (ifst.eof()) {
+			cout << "INFORMATION: the end of file." << endl;
+			return 0;
+		}
 	};
 	// Вывод параметров текущей мудрости в поток
 	void Out(wisdom &s, ofstream &ofst) {
@@ -366,10 +227,17 @@ namespace collection_of_wisdom {
 				return "error";
 			}
 		}
-		catch (int Position) {// если Position != 0
-			cout << "WARNING: unexpected format of data (" << Line.substr(0, Position + static_cast<int>(Text.length())) << ")." << endl;
-			Data = Line.substr(Position + static_cast<int>(Text.length()), static_cast<int>(Line.length()) - 1);
-			return Data;
+		catch (int Position) {
+			if (Position != -1) { //если данные маленько сдвинуты
+				cout << "WARNING: unexpected format of data (" << Line.substr(0, Position + static_cast<int>(Text.length())) << ")." << endl;
+				Data = Line.substr(Position + static_cast<int>(Text.length()), static_cast<int>(Line.length()) - 1);
+				return Data;
+			}
+			else if (Position == -1) { //если данные введены неправильно
+				cout << "ERROR: incorrect data format." << endl;
+				return "error";
+			}
+
 		}
 	};	char& CheckForOverflow(string & Data, char char_text[], ifstream &ifst, int Len)
 	{
